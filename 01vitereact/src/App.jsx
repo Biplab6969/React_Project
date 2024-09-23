82a91db03b9886a4ad568fbb00b1446452849488
biplab <jbiplab80@gmail.com>
2024-09-23 07:58:17 +0530

    React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+ const username = "chai aur code"
+  return (
+    <>
+    <Chai/>
+    <p1>test para</p1>
+    <h1>chai aur react{username}</h1>
+    </>
+   
+  )
+}
+
+export default App
